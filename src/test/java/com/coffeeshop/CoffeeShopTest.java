f192040ced648d983ec57120367c344156412050
LINLING GUO <llguo10300@gmail.com>
2025-04-03 16:01:14 +0100

    enhance text

--- src/test/java/com/coffeeshop/CoffeeShopTest.java
+++ src/test/java/com/coffeeshop/CoffeeShopTest.java
@@ -1,3 +1,12 @@
+package com.coffeeshop;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 public class CoffeeShopTest extends TestCase {
 
     public CoffeeShopTest(String testName) {
@@ -16,46 +25,57 @@ public class CoffeeShopTest extends TestCase {
 
     public void testAmericanoOption() {
         simulateInputAndRun("10\n10\n10\namericano\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testMochaOption() {
         simulateInputAndRun("10\n10\n10\nmocha\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testIceTeaOption() {
         simulateInputAndRun("10\n10\n10\nice tea\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testInvalidCoffeeInput() {
         simulateInputAndRun("10\n10\n10\nlatte\n1\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testNegativeStockTriggersWarning() {
         simulateInputAndRun("-5\n10\n10\n1\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testInvalidStockInputTriggersCatch() {
-        simulateInputAndRun("abc\n10\n10\n1\n1\ny\n");
+        simulateInputAndRun("abc\n10\n10\n10\n1\n1\ny\n");
+        assertTrue(true);
     }
 
     public void testNegativeQuantityTriggersWarning() {
         simulateInputAndRun("10\n10\n10\n1\n-2\n2\ny\n");
+        assertTrue(true);
     }
 
     public void testInvalidQuantityInputTriggersCatch() {
         simulateInputAndRun("10\n10\n10\n1\nabc\n2\ny\n");
+        assertTrue(true);
     }
 
     public void testBuyTooMuchCoffee() {
         simulateInputAndRun("2\n10\n10\n1\n99\n1\n2\ny\n");
+        assertTrue(true);
     }
 
     public void testFinishInputInvalid() {
         simulateInputAndRun("10\n10\n10\n1\n1\nmaybe\ny\n");
+        assertTrue(true);
     }
 
     public void testQuitImmediately() {
         simulateInputAndRun("10\n10\n10\n4\n1\n1\ny\n");
+        assertTrue(true);
     }
 
     private void simulateInputAndRun(String input) {
